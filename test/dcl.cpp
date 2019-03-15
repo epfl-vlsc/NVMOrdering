fe1f9f86dc55f59f0c21925f5e8b020d4445ec24
daksunt <davidteksenaksun@gmail.com>
2019-03-15 09:43:04 +0100

    test branch for dcl

--- test/dcl.cpp
+++ test/dcl.cpp
@@ -5,36 +5,43 @@
 
 //different cache line placement analysis
 
-void vfence(){
+void vfence()
+{
 	std::atomic_thread_fence(std::memory_order_release);
 }
 
-void pfence(){
+void pfence()
+{
 	_mm_sfence();
 }
-void clflush(void const *p){
+void clflush(void const *p)
+{
 	_mm_clflush(p);
 }
 
-void assign(int* a, int val){
+void assign(int *a, int val)
+{
 	*a = val;
 }
 
-
-struct ParentA{
+struct ParentA
+{
 	int a;
 };
 
-class ChildB{
+class ChildB
+{
 	ParentA a;
 	int b;
 };
 
-struct LogEntry{
+struct LogEntry
+{
 	pdcl(LogEntry.valid) int data;
 	pcheck() int valid;
 
-	LogEntry(){
+	LogEntry()
+	{
 		data = 0;
 		clflush(&data);
 		pfence();
@@ -43,23 +50,28 @@ struct LogEntry{
 		pfence();
 	}
 
-	void setData(int data_){
+	void setData(int data_)
+	{
 		data = 1;
 	}
 
-	void setValid(int valid_){
+	void setValid(int valid_)
+	{
 		valid = 1;
 	}
 
-	void flushData(){
+	void flushData()
+	{
 		clflush(&data);
 	}
 
-	void flushValid(){
+	void flushValid()
+	{
 		clflush(&valid);
 	}
 
-	void persistent_code correctModelMethod(){
+	void persistent_code correctModelMethod()
+	{
 		data = 1;
 		clflush(&data);
 		pfence();
@@ -68,7 +80,8 @@ struct LogEntry{
 		pfence();
 	}
 
-	void persistent_code finalFenceMissing(){
+	void persistent_code finalFenceMissing()
+	{
 		data = 1;
 		clflush(&data);
 		pfence();
@@ -77,9 +90,9 @@ struct LogEntry{
 	}
 };
 
-
-void persistent_code correctModelFunction(){
-	LogEntry* entry = new LogEntry;
+void persistent_code correctModelFunction()
+{
+	LogEntry *entry = new LogEntry;
 	entry->setData(1);
 	entry->flushData();
 	pfence();
@@ -88,12 +101,28 @@ void persistent_code correctModelFunction(){
 	pfence();
 }
 
-void persistent_code writeToValid(){
-	LogEntry* entry = new LogEntry;
+void persistent_code wrongBranchFunction(bool useNVM)
+{
+	LogEntry *entry = new LogEntry;
+	entry->setData(1);
+	entry->flushData();
+	pfence();
+	if (useNVM)
+	{
+		entry->setValid(1);
+		entry->flushValid();
+		pfence();
+	}
+}
+
+void persistent_code writeToValid()
+{
+	LogEntry *entry = new LogEntry;
 	entry->setValid(1);
 }
 
-void megaman(){
+void megaman()
+{
 	int zeman;
 	LogEntry entry;
 	entry.setData(1);
