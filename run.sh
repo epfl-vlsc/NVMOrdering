98e052bf835b6a4789beb5b64b08ffe1e4ab1946
daksunt <davidteksenaksun@gmail.com>
2019-03-16 16:00:15 +0100

    enable bug report generation for html

--- run.sh
+++ run.sh
@@ -40,6 +40,26 @@ run_tool2(){
     cd ..
 }
 
+run_scanbuild(){
+    #todo -analyzer-opt-analyze-headers
+    cd build
+    OUT_DIR=../output
+    #rm -r $OUT_DIR/*
+    scan-build -o $OUT_DIR \
+        -disable-checker alpha -disable-checker apiModeling \
+        -disable-checker valist -disable-checker cplusplus \
+        -disable-checker deadcode -disable-checker debug \
+        -disable-checker llvm -disable-checker nullability  \
+        -disable-checker optin -disable-checker security  \
+        -disable-checker osx -disable-checker unix -disable-checker core \
+        clang++ \
+        -fsyntax-only -fplugin=lib/liborderingchecker.so \
+        -Xclang -analyzer-max-loop -Xclang 2 \
+        -Xclang -analyze -Xclang -analyzer-checker=nvm.orderingchecker \
+        $test_file -Xclang -analyzer-display-progress
+    cd ..
+}
+
 dump_ast(){
     cd build
     clang++ -Xclang -ast-dump -fsyntax-only $test_file > ../test/ast.txt
@@ -53,6 +73,9 @@ dump_ast(){
 if [ "$mode" == "run" ] ;then
 	run_make
 	run_tool
+elif [ "$mode" == "scan" ] ;then
+	run_make
+	run_scanbuild
 elif [ "$mode" == "build" ] ;then
     rm -r build
 	create_build
