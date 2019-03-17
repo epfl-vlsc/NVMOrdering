f532dff91bf49272c5b5c730616d5d301122f481
daksunt <davidteksenaksun@gmail.com>
2019-03-17 16:05:50 +0100

    put everything into comm header to share across checks

--- lib/orderingchecker/Common.h
+++ lib/orderingchecker/Common.h
@@ -9,34 +9,72 @@
 
 namespace clang::ento::nvm {
 
-const FunctionDecl* getFuncDecl(const Decl* BD);
+const FunctionDecl* getFuncDecl(const Decl* BD) {
+  if (const FunctionDecl* D = dyn_cast_or_null<FunctionDecl>(BD)) {
+    return D;
+  }
+  llvm::report_fatal_error("use only for functions");
+  return nullptr;
+}
 
-const FunctionDecl* getFuncDecl(const CallEvent& Call);
+const FunctionDecl* getFuncDecl(const CallEvent& Call) {
+  const Decl* BD = Call.getDecl();
+  return getFuncDecl(BD);
+}
 
-const FunctionDecl* getFuncDecl(const LocationContext* LC);
+const FunctionDecl* getFuncDecl(const LocationContext* LC) {
+  const Decl* BD = LC->getDecl();
+  return getFuncDecl(BD);
+}
 
-const DeclaratorDecl* getDeclaratorDecl(const Decl* BD);
+const DeclaratorDecl* getDeclaratorDecl(const Decl* BD) {
+  if (const DeclaratorDecl* D = dyn_cast_or_null<DeclaratorDecl>(BD)) {
+    return D;
+  }
+  llvm::report_fatal_error("All calls have function declaration");
+  return nullptr;
+}
 
-bool isTopFunction(CheckerContext& C);
+bool isTopFunction(CheckerContext& C) { return C.inTopFrame(); }
 
 class MaskWalker : public ConstStmtVisitor<MaskWalker> {
   bool usesMask_;
   StringRef mask_;
 
 public:
-  MaskWalker(StringRef mask);
+  void VisitDeclRefExpr(const DeclRefExpr* DRE) {
+  StringRef currentMask =
+      DRE->getNameInfo().getName().getAsIdentifierInfo()->getName();
+  if (currentMask.equals(mask_)) {
+    usesMask_ = true;
+  }
+}
 
-  void VisitDeclRefExpr(const DeclRefExpr* DRE);
+MaskWalker(StringRef mask) : usesMask_(false), mask_(mask) {}
 
-  void VisitStmt(const Stmt* S);
+void VisitStmt(const Stmt* S) { VisitChildren(S); }
 
-  void VisitUnaryOperator(const UnaryOperator* UOp);
+void VisitUnaryOperator(const UnaryOperator* UOp) {
+  // using another mask
+  usesMask_ = false;
+}
 
-  void VisitChildren(const Stmt* S);
+void VisitChildren(const Stmt* S) {
+  for (Stmt::const_child_iterator I = S->child_begin(), E = S->child_end();
+       I != E; ++I) {
+    if (const Stmt* Child = *I) {
+      Visit(Child);
+    }
+  }
+}
 
-  bool usesMask();
+bool usesMask() { return usesMask_; }
 };
 
-bool usesMask(const Stmt* S, StringRef mask);
+bool usesMask(const Stmt* S, StringRef mask) {
+  MaskWalker maskWalker(mask);
+  maskWalker.Visit(S);
+  return maskWalker.usesMask();
+}
 
 } // namespace clang::ento::nvm
