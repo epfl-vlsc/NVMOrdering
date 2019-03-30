e95a261f5467b1e8a0771be43d2ec926edfacf1a
daksunt <davidteksenaksun@gmail.com>
2019-03-30 21:38:06 +0100

    add mask test

---
+++ test/sclm.cpp
@@ -0,0 +1,67 @@
+#include "annot.h"
+#include <atomic>
+#include <stdio.h>
+#include <xmmintrin.h>
+
+// different cache line placement analysis
+
+void vfence() { std::atomic_thread_fence(std::memory_order_release); }
+void pfence() { _mm_sfence(); }
+void clflush(void const* p) { _mm_clflush(p); }
+
+struct SimpleSclm {
+  enum {
+    MASK = 7,
+  };
+  psclm() int data;
+
+  void writeData() { data = (data & MASK) | 1; }
+
+  void writeValid() { data = (data & ~MASK) | 1; }
+
+  void persistent_code correct() {
+    writeData();
+    vfence();
+    writeValid();
+  }
+
+  void persistent_code correctPfence() {
+    writeData();
+    pfence();
+    writeValid();
+  }
+
+  void persistent_code initWriteDataTwice() {
+    writeData();
+    writeData();
+    vfence();
+    writeValid();
+  }
+
+  void persistent_code notFencedData() {
+    writeData();
+    writeValid();
+  }
+
+  void persistent_code initValid() {
+    writeValid();
+    writeData();
+    vfence();
+    writeValid();
+  }
+
+  void persistent_code beforeValidWriteData() {
+    writeData();
+    vfence();
+    writeData();
+    writeValid();
+  }
+
+  void persistent_code branch(bool useNvm) {
+    writeData();
+    if (useNvm) {
+      vfence();
+      writeValid();
+    }
+  }
+};
