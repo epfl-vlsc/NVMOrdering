1555d3cc5591373c3090b9c33f6067703fb20ed1
daksunt <davidteksenaksun@gmail.com>
2019-03-31 13:08:09 +0200

    test transitive properties

--- test/sclm.cpp
+++ test/sclm.cpp
@@ -65,3 +65,73 @@ struct SimpleSclm {
     }
   }
 };
+
+struct TransitiveSclm {
+  enum {
+    MASK = 7,
+  };
+  psclm(TransitiveSclm::valid) int chunk;
+  int valid;
+
+  void writeCD() { chunk = (chunk & MASK) | 1; }
+
+  void writeCV() { chunk = (chunk & ~MASK) | 1; }
+
+  void writeValid() { valid = 1; }
+
+  void persistent_code correct() {
+    writeCD();
+    vfence();
+    writeCV();
+    vfence();
+    writeValid();
+  }
+
+  void persistent_code correctPfence() {
+    writeCD();
+    pfence();
+    writeCV();
+    pfence();
+    writeValid();
+  }
+
+  void persistent_code firstFenceMissing() {
+    writeCD();
+    writeCV();
+    vfence();
+    writeValid();
+  }
+
+  void persistent_code secondFenceMissing() {
+    writeCD();
+    vfence();
+    writeCV();
+    writeValid();
+  }
+
+  void persistent_code initValid() {
+    writeValid();
+    writeCD();
+    vfence();
+    writeCV();
+    vfence();
+    writeValid();
+  }
+
+  void persistent_code initCV() {
+    writeCV();
+    writeCD();
+    vfence();
+    writeCV();
+    vfence();
+    writeValid();
+  }
+
+  void persistent_code writeValidBeforeCV() {
+    writeCD();
+    vfence();
+    writeValid();
+    vfence();
+    writeCV();
+  }
+};
