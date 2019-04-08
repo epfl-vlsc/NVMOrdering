23f1b0a20800e6bf4f3b510af83ca6d5d15d0cef
daksunt <davidteksenaksun@gmail.com>
2019-04-08 20:53:31 +0200

    add state descriptions for pmdk tx checker

--- lib/txpchecker/States.h
+++ lib/txpchecker/States.h
@@ -6,6 +6,10 @@ namespace clang::ento::nvm {
 struct WriteState {
 private:
   enum Kind { Init, Logged, Written } K;
+  static constexpr const char* Str[] = {"Init", "Logged", "Written"};
+  static constexpr const char* Exp[] = {"data is init", "data is logged",
+                                        "data is written"};
+
   WriteState(Kind InK) : K(InK) {}
 
 public:
@@ -17,6 +21,11 @@ public:
   static WriteState getLogged() { return WriteState(Logged); }
   static WriteState getWritten() { return WriteState(Written); }
 
+  int getStateKind() const { return static_cast<int>(K); }
+
+  const char* getStateName() const { return Str[K]; }
+  const char* getExplanation() const { return Exp[K]; }
+
   bool operator==(const WriteState& X) const { return K == X.K; }
   void Profile(llvm::FoldingSetNodeID& ID) const { ID.AddInteger(K); }
 };
@@ -24,5 +33,5 @@ public:
 } // namespace clang::ento::nvm
 
 REGISTER_TRAIT_WITH_PROGRAMSTATE(TxCounter, unsigned)
-REGISTER_MAP_WITH_PROGRAMSTATE(PMap, const clang::ento::MemRegion*,
+REGISTER_MAP_WITH_PROGRAMSTATE(PMap, const clang::NamedDecl*,
                                clang::ento::nvm::WriteState)
