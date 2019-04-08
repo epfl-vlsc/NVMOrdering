d6aa077534f2ff20a36356529da1ca34494aa14d
daksunt <davidteksenaksun@gmail.com>
2019-04-08 20:52:39 +0200

    use base bug reporter, shared across checkers

--- lib/writechecker/StateMachine/WriteBugReporter.h
+++ lib/writechecker/StateMachine/WriteBugReporter.h
@@ -1,47 +1,11 @@
 #pragma once
 #include "Common.h"
 #include "clang/StaticAnalyzer/Core/BugReporter/BugType.h"
+#include "BaseReporter.h"
 
 namespace clang::ento::nvm {
 
-using BugPtr = std::unique_ptr<BugType>;
 const std::string WriteError = "NVM Write Error";
-
-class BaseReporter {
-protected:
-  std::string getErrorMessage(CheckerContext& C, const char* D,
-                              const char* msg) const {
-    const FunctionDecl* FD = getTopFunction(C);
-    std::string sbuf;
-    llvm::raw_string_ostream ErrorOs(sbuf);
-    ErrorOs << "At: " << FD->getQualifiedNameAsString() << " " << msg;
-
-    const Decl* BD = (const Decl*)D;
-    if (const ValueDecl* VD = dyn_cast_or_null<ValueDecl>(BD)) {
-      ErrorOs << " " << VD->getNameAsString();
-    }
-
-    return ErrorOs.str();
-  }
-
-  void reportDirect(SVal* Loc, const std::string& ErrMsg,
-                    const ExplodedNode* const EN, const BugPtr& bugPtr,
-                    BugReporter& BReporter) const {
-    auto Report = llvm::make_unique<BugReport>(*bugPtr, ErrMsg, EN);
-    if (Loc) {
-      Report->markInteresting(*Loc);
-    }
-    BReporter.emitReport(std::move(Report));
-  }
-
-public:
-  void report(CheckerContext& C, const char* D, const char* msg, SVal* Loc,
-              const ExplodedNode* const EN, const BugPtr& bugPtr) const {   
-    BugReporter& BReporter = C.getBugReporter();
-    std::string ErrMsg = getErrorMessage(C, D, msg);
-    reportDirect(Loc, ErrMsg, EN, bugPtr, BReporter);
-  }
-};
 
 class WDBugs {
 public:
