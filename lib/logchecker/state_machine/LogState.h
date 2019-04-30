741966f13b16058581699978a2bd02853159db43
daksunt <davidteksenaksun@gmail.com>
2019-04-30 11:12:12 +0200

    add new logstate

--- lib/logchecker/state_machine/LogState.h
+++ lib/logchecker/state_machine/LogState.h
@@ -5,15 +5,17 @@ namespace clang::ento::nvm {
 
 struct LogState {
 private:
-  enum Kind { Logged, Written } K;
+  enum Kind { Init, Logged, Written } K;
   LogState(Kind InK) : K(InK) {}
 
 public:
   bool isLogged() const { return K == Logged; }
   bool isWritten() const { return K == Written; }
+  bool isInit() const { return K == Init; }
 
   static LogState getLogged() { return LogState(Logged); }
   static LogState getWritten() { return LogState(Written); }
+  static LogState getInit() { return LogState(Init); }
 
   bool operator==(const LogState& X) const { return K == X.K; }
   void Profile(llvm::FoldingSetNodeID& ID) const { ID.AddInteger(K); }
